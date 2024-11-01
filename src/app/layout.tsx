61669bf5175adca9eb5e18c0db30e6a5ca54f6c9
Fatemeh <fatemeh.saedchi@gmail.com>
2024-11-01 19:30:52 +0330

    TanStack Query Installation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./global.css";
+import QueryProvider from './context/QueryClientProvider';
 
 export default function RootLayout({children}: {
     children: React.ReactNode
 }) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>
+        <QueryProvider>
+            {children}
+        </QueryProvider>
+        </body>
         </html>
     )
 }
